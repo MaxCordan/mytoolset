74238a79889a3fca4d51ff9eddd57a82a0f48797
Maksym Puzik <maksym_puzik@epam.com>
2023-08-11 09:11:04 +0000

    Install additional scripts

---
+++ install_plugins.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
