08c3de2a2a62b046bb37653c2db36002e36fb9f3
Maksym Puzik <maxcordan@gmail.com>
2023-08-31 14:44:47 +0000

    update scripts

---
+++ install_config.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -sf mytoolset/.zshrc ~/.zshrc
+ln -sf mytoolset/.p10k.zsh ~/.p10k.zsh
--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
