c600d9a0bf656580aa24ede050090fad425cd39d
Maksym Puzik <maxcordan@gmail.com>
2024-01-02 19:55:03 +0100

    Add AWS CLI install

--- 1_install_dependancies.sh
+++ 1_install_dependancies.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-sudo apt-get install git curl wget zsh
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y git curl wget zsh unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip && sudo ./aws/install
+rm -rf awscliv2.zip aws
